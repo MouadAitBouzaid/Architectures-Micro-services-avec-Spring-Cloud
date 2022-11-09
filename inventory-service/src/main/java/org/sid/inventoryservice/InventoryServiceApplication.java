4f1d95da1a40e544844aabf2d111c452fde07b77
Mouad AIT BOUZAID <77202158+MouadAitBouzaid@users.noreply.github.com>
2022-11-09 15:55:27 +0100

    0.0.2

--- inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,9 @@
 package org.sid.inventoryservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +11,12 @@ public class InventoryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
+    @Bean
+    CommandLineRunner start(){
+        return args -> {
 
+        };
+    }
 }
+
+
