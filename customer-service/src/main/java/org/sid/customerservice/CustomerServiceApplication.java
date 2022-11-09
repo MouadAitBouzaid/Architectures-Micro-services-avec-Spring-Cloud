4019e3c4c65796cccce0677d14d0cc4293ae6986
Mouad AIT BOUZAID <77202158+MouadAitBouzaid@users.noreply.github.com>
2022-11-09 15:43:03 +0100

    0.0.2
    La mise en place des micro services

---
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,28 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository){
+        return  args -> {
+            customerRepository.save(new Customer(null, "salah", "salah@gmail.com"));
+            customerRepository.save(new Customer(null, "Youssef", "Youssef@gmail.com"));
+            customerRepository.save(new Customer(null, "Mouad", "Mouad@gmail.com"));
+            customerRepository.findAll().forEach(c->{
+                System.out.println(c.toString());
+            });
+        };
+    }
+
+}
