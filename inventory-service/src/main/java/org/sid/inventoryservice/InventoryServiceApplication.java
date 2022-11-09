a51368efcc6df9ab9ce5efbb37bbe11bb43784d8
Mouad AIT BOUZAID <77202158+MouadAitBouzaid@users.noreply.github.com>
2022-11-09 21:25:38 +0100

    0.0.4
    implimentation du product-service

--- inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -1,5 +1,7 @@
 package org.sid.inventoryservice;
 
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repository.productRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,15 +10,19 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class InventoryServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(InventoryServiceApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(InventoryServiceApplication.class, args);
     }
     @Bean
-    CommandLineRunner start(){
+    CommandLineRunner start(productRepository productRepository){
         return args -> {
-
+            productRepository.save(new Product(null, "Oridnateur", 788,12 ));
+            productRepository.save(new Product(null, "SmartWatch", 400,200 ));
+            productRepository.save(new Product(null, "SmartPhone", 520,186 ));
+            productRepository.findAll().forEach(p->{
+                System.out.println(p.getName());
+            });
         };
-    }
+    };
 }
 
 
