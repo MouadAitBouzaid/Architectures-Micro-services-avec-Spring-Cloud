8f0be486fc9682fd058473e15fcfa4e20545e99c
Mouad AIT BOUZAID <77202158+MouadAitBouzaid@users.noreply.github.com>
2022-11-30 14:52:16 +0100

    2.9.6

--- billing-service/src/main/java/com/example/billingservice/web/BillingRestController.java
+++ billing-service/src/main/java/com/example/billingservice/web/BillingRestController.java
@@ -1,4 +1,22 @@
 package com.example.billingservice.web;
 
+import com.example.billingservice.entities.Bill;
+import com.example.billingservice.feign.CustomerRestClient;
+import com.example.billingservice.feign.ProductItemRestClient;
+import com.example.billingservice.repository.BillRepository;
+import com.example.billingservice.repository.ProductItemRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BillingRestController {
+    private BillRepository billRepository;
+    private ProductItemRepository productItemRepository;
+    private CustomerRestClient customerRestClient;
+    private ProductItemRestClient productItemRestClient;
+
+    @GetMapping(path="/fullBill/{id}")
+    public Bill getBill(Long id){
+        Bill bill
+    }
 }
